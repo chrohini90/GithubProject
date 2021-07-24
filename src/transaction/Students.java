7a9bdeaa2568896f68159136cac744721322f587
19407 <19407@LAPTOP-F4SI18QJ>
2021-07-24 12:53:17 -0500

    Completed

---
+++ src/transaction/Students.java
@@ -0,0 +1,21 @@
+package transaction;
+
+public class Students {
+	private int id;
+	String name;
+	String course;
+	int grade;
+	private int Paidfees;
+	private int Totalfees;
+	
+		Students(int id,String name,int grade){
+			this.id=id;
+			this.name = name;
+			this.grade = grade;
+			
+			
+	}
+	
+	
+
+}
